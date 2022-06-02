73e3a31d1584b88ef86845605f5c15d1081516c4
Legend Tree <lyuchuanqi@qq.com>
2022-06-02 13:30:07 +0800

    修改框架

---
+++ src/pages/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+  Hello World
+</template>
+    
+<script lang='ts'>
+import { defineComponent } from 'vue'
+    
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
